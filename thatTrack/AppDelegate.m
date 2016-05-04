d285c01489f674a8ef86437df1fd824660897f33
opusmagnum77 <daniel.adeyanju@gmail.com>
2016-05-04 00:38:17 -0400

    Update gitignore w secrets

--- thatTrack/AppDelegate.m
+++ thatTrack/AppDelegate.m
@@ -1,2 +1,74 @@
 #import <Spotify/Spotify.h>
 #import "AppDelegate.h"
+
+@interface AppDelegate ()
+@property (nonatomic, strong) SPTSession *session;
+@property (nonatomic, strong) SPTAudioStreamingController *player;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[SPTAuth defaultInstance] setClientID:@"Your-Client-Id"];
+    [[SPTAuth defaultInstance] setRedirectURL:[NSURL URLWithString:@"Your-Callback-URL"]];
+    [[SPTAuth defaultInstance] setRequestedScopes:@[SPTAuthStreamingScope]];
+    
+    // Construct a login URL and open it
+    NSURL *loginURL = [[SPTAuth defaultInstance] loginURL];
+    
+    // Opening a URL in Safari close to application launch may trigger
+    // an iOS bug, so we wait a bit before doing so.
+    [application performSelector:@selector(openURL:)
+                      withObject:loginURL afterDelay:0.1];
+    
+    return YES;
+}
+
+// Handle auth callback
+-(BOOL)application:(UIApplication *)application
+           openURL:(NSURL *)url
+ sourceApplication:(NSString *)sourceApplication
+        annotation:(id)annotation {
+    
+    // Ask SPTAuth if the URL given is a Spotify authentication callback
+    if ([[SPTAuth defaultInstance] canHandleURL:url]) {
+        [[SPTAuth defaultInstance] handleAuthCallbackWithTriggeredAuthURL:url callback:^(NSError *error, SPTSession *session) {
+            
+            if (error != nil) {
+                NSLog(@"*** Auth error: %@", error);
+                return;
+            }
+            
+            // Call the -playUsingSession: method to play a track
+            [self playUsingSession:session];
+        }];
+        return YES;
+    }
+    
+    return NO;
+}
+
+-(void)playUsingSession:(SPTSession *)session {
+    
+    // Create a new player if needed
+    if (self.player == nil) {
+        self.player = [[SPTAudioStreamingController alloc] initWithClientId:[SPTAuth defaultInstance].clientID];
+    }
+    
+    [self.player loginWithSession:session callback:^(NSError *error) {
+        if (error != nil) {
+            NSLog(@"*** Logging in got error: %@", error);
+            return;
+        }
+        
+        NSURL *trackURI = [NSURL URLWithString:@"spotify:track:58s6EuEYJdlb0kO7awm3Vp"];
+        [self.player playURIs:@[ trackURI ] fromIndex:0 callback:^(NSError *error) {
+            if (error != nil) {
+                NSLog(@"*** Starting playback got error: %@", error);
+                return;
+            }
+        }];
+    }];
+}
+
+@end
---
+++ thatTrack/Secrets.h
@@ -0,0 +1,13 @@
+//
+//  Secrets.h
+//  thatTrack
+//
+//  Created by Daniel Adeyanju on 5/4/16.
+//  Copyright © 2016 Daniel Adeyanju. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Secrets : NSObject
+
+@end
